177da43e601eff6a8eab39e862fae5a5858fa56c
Kamil <kamil.suwiczak@wp.pl>
2025-12-17 20:19:48 +0100

    added basic usage of network_client

--- client/main_lobby.py
+++ client/main_lobby.py
@@ -4,17 +4,23 @@ from views.set_player_nick_view import NickSetPlayerView
 from views.set_host_nick_view import NickSetHostView
 from views.game_view import GameView
 from views.room_view import RoomView
+from network_client import NetworkClient
 
 from dotenv import load_dotenv
 import os
 load_dotenv()
+
+#zeby odpalic w .env musi byc:
+# WINDOW_SIZE,
+# SERVER_IP,
+# SERVER_PORT
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         
         self.title("Klient Gry")
-        self.geometry(os.getenv("WINDOW_SIZE", "1000x600"))
+        self.geometry(os.getenv("WINDOW_SIZE"))
         
         self.container = ctk.CTkFrame(self)
         self.container.pack(side="top", fill="both", expand=True)
@@ -31,6 +37,27 @@ class App(ctk.CTk):
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame("MenuView")
+        self.network_client = NetworkClient(os.getenv("SERVER_IP"), int(os.getenv("SERVER_PORT")), self.on_message_received)
+        self.network_client.connect()
+
+    def handle_server_message(self, message):
+        self.after(0, lambda: self._process_message(message))
+    
+    def _process_message(self, message):
+        if message.startswith("PLAYERS:"):
+            raw_data = message.split(":")[1]
+            players_list = raw_data.split(",")
+            
+            if "RoomView" in self.frames:
+                self.frames["RoomView"].update_players(players_list)
+
+        elif message == "START_GAME":
+            self.show_frame("GameView")
+
+        elif message.startswith("WORD:"):
+            word = message.split(":")[1]
+            if "GameView" in self.frames:
+                self.frames["GameView"].update_word(word)
 
     def show_frame(self, page_name):
         """Funkcja, która wyciąga dany widok na wierzch"""
